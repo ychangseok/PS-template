eaa6727e99a90fa56daf1a90f5d99a8b4d0b49c3
ychangseok <147029297+ychangseok@users.noreply.github.com>
2026-02-17 19:18:49 +0900

    Create manacher.cpp

---
+++ String/manacher.cpp
@@ -0,0 +1,37 @@
+vector<int> manacher(string s){
+    string ss;
+    for (char c : s){
+        ss.push_back('#');
+        ss.push_back(c);
+    }
+    ss.push_back('#');
+
+    int r = -1;
+    int c = -1;
+    int ll = ss.length();
+
+    vector<int> res(ll);
+
+    for (int i = 0; i < ll; i++){
+        if (i <= r){
+            res[i] = min(r-i, res[c + (c-i)]);
+        }
+
+        while (i + res[i] + 1 < ll && i - res[i] - 1 >= 0 && ss[i + res[i] + 1] == ss[i - res[i] - 1]){
+            res[i]++;
+        }
+
+        if (i + res[i] > r){
+            c = i;
+            r = i + res[i];
+        }
+    }
+
+    // cout << ss << '\n';
+    // for (int i : res){
+    //     cout << i;
+    // }
+    // cout << '\n';
+
+    return res;
+}
