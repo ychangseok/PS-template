abda7006917fd6b71caba8b2f233df5e4fa26a44
ychangseok <147029297+ychangseok@users.noreply.github.com>
2026-03-11 20:50:53 +0900

    Update angle_sort.cpp

--- Geometry/angle_sort.cpp
+++ Geometry/angle_sort.cpp
@@ -1,9 +1,9 @@
 // 360 degree ccw angle sort centered on p
 sort(all(pts),
-    [&p](const PT &p1, const PT &p2) -> bool{
-        if (p2 == p) return false;
-        if (p1 == p) return true;
-        if (p1 > p != p2 > p) return p1 < p2;
-        return ccw(p, p1, p2) > 0;
+    [&C](const PT &p1, const PT &p2) -> bool{
+        if (p2 == C) return false;
+        if (p1 == C) return true;
+        if (p1 > C != p2 > C) return p1 < p2;
+        return ccw(C, p1, p2) > 0;
     }
 );
